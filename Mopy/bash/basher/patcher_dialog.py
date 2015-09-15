3fe72014e082cff00855ee10bb9cc29fad164324
Metallicow <metaliobovinus@gmail.com>
2015-09-14 19:08:32 -0500

    event.Skip in patcher dialog OnSize - TODO: test

--- Mopy/bash/basher/patcher_dialog.py
+++ Mopy/bash/basher/patcher_dialog.py
@@ -466,7 +466,8 @@ class PatchDialog(balt.Dialog):
         self.gExecute.Enable(False)
 
     #--GUI --------------------------------
-    def OnSize(self,event): ##: needed ? event.Skip() ??
+    def OnSize(self,event):
+        event.Skip()
         balt.sizes[self.__class__.__name__] = tuple(self.GetSize())
         self.Layout()
         self.currentPatcher.Layout()
