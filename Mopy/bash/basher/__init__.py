42fcf046306f3af8d40300053c8b6a068fb1ddb5
Metallicow <metaliobovinus@gmail.com>
2015-09-17 18:03:02 -0500

    kill gizmos; ThinSplitterWindow to SplitterWindow [squashed with fix]
    Phoenix compatability; ..also, I don't see anything special that is
    being used anywhere that SplitterWindow doesn't have already.
    
     # This is the 2nd commit message:
    
    Fix the last commit. merge positioning got borked
    
    ...somehow I clicked the wrong lines or something...
    squashify this with the last commit for killing gizmo.

--- Mopy/bash/basher/__init__.py
+++ Mopy/bash/basher/__init__.py
@@ -63,7 +63,7 @@ from functools import partial
 
 #--wxPython
 import wx
-import wx.gizmos
+### import wx.gizmos  # Get rid of ThinSplitterWindow once and for all. Use wx.SplitterWindow instead
 
 #--Localization
 #..Handled by bosh, so import that.
@@ -248,8 +248,7 @@ class SashPanel(_DetailsViewMixin, NotebookPanel):
     def __init__(self, parent, sashGravity=0.5, isVertical=True,
                  style=splitterStyle):
         NotebookPanel.__init__(self, parent)
-        self.splitter = splitter = wx.gizmos.ThinSplitterWindow(self,
-                                                                style=style)
+        self.splitter = splitter = wx.SplitterWindow(self, style=style)
         self.left = wx.Panel(splitter)
         self.right = wx.Panel(splitter)
         if isVertical:
@@ -1124,8 +1123,7 @@ class _SashDetailsPanel(_EditableMixin, SashPanel):
         SashPanel.__init__(self, parent, sashGravity=1.0, isVertical=False,
                            style=wx.SW_BORDER | splitterStyle)
         self.top, self.bottom = self.left, self.right
-        self.subSplitter = wx.gizmos.ThinSplitterWindow(self.bottom,
-                                                        style=splitterStyle)
+        self.subSplitter = wx.SplitterWindow(self.bottom, style=splitterStyle)
         self.masterPanel = wx.Panel(self.subSplitter)
         _EditableMixin.__init__(self, self.masterPanel)
 
@@ -2475,9 +2473,9 @@ class InstallersPanel(SashTankPanel):
         SashTankPanel.__init__(self,data,parent)
         left,right = self.left,self.right
         self.commentsSplitter = commentsSplitter = \
-            wx.gizmos.ThinSplitterWindow(right, style=splitterStyle)
-        subSplitter = wx.gizmos.ThinSplitterWindow(commentsSplitter, style=splitterStyle)
-        checkListSplitter = wx.gizmos.ThinSplitterWindow(subSplitter, style=splitterStyle)
+            wx.SplitterWindow(right, style=splitterStyle)
+        subSplitter = wx.SplitterWindow(commentsSplitter, style=splitterStyle)
+        checkListSplitter = wx.SplitterWindow(subSplitter, style=splitterStyle)
         #--Refreshing
         self.refreshed = False
         self.refreshing = False
