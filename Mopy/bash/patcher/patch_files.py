ad6a51196dbfd4c634111d89ad60f8a4bb92da30
Sharlikran <danielhmpdx16@gmail.com>
2015-11-22 20:38:54 -0800

    Added Fallout default bodyTags

--- Mopy/bash/patcher/patch_files.py
+++ Mopy/bash/patcher/patch_files.py
@@ -59,7 +59,10 @@ class _PFile(object):
         self.compiledAllMods = []
         self.patcher_mod_skipcount = defaultdict(lambda: defaultdict(int))
         #--Config
-        self.bodyTags = 'ARGHTCCPBS' #--Default bodytags
+        if bush.game.fsName in (u'FalloutNV', u'Fallout3',):
+            self.bodyTags = 'HAGPBFE' #--Default bodytags
+        else: #--Default bodytags
+            self.bodyTags = 'ARGHTCCPBS'
         #--Mods
         dex = load_order.loIndexCached
         loadMods = [name for name in load_order.activeCached() if
