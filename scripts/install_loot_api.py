6fbc8e11448193567f424bd398394658a6b95413
Oliver Hamlet <oliver.hamlet@gmail.com>
2016-10-17 08:12:16 +0100

    Add LOOT API install script:
    Downloads and installs the API python wrapper from Bintray at the
    moment: following LOOT v0.10.0's release this will be updated to
    download from GitHub instead.
    
    - Update LOOT API installation script
    
    It now downloads and installs the MSVC 2015 redistributable if
    necessary, and saves downloaded files to the OS temporary directory.
    
    - Wrap install script registry access in try/catch
    
    If access fails (eg. because the key doesn't exist) the functions throw,
    so assume in that case that the runtime needs to be installed.

---
+++ scripts/install_loot_api.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+import sys
+import tempfile
+import urllib
+import _winreg
+
+try:
+    sys.path.append('../Mopy/bash/compiled')
+    import loot_api
+except ImportError:
+    pass
+
+def isMSVCRedistInstalled(majorVersion, minorVersion, buildVersion):
+    subKey = 'SOFTWARE\\Microsoft\\VisualStudio\\14.0\\VC\\Runtimes\\x86'
+
+    try:
+        keyHandle = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, subKey)
+
+        runtimeInstalled = _winreg.QueryValueEx(keyHandle, 'Installed')[0]
+        installedMajorVersion = _winreg.QueryValueEx(keyHandle, 'Major')[0]
+        installedMinorVersion = _winreg.QueryValueEx(keyHandle, 'Minor')[0]
+        installedBuildVersion = _winreg.QueryValueEx(keyHandle, 'Bld')[0]
+
+        if runtimeInstalled != 0:
+            print 'Found MSVC 2015 redistributable version %s.%s.%s' % (installedMajorVersion, installedMinorVersion, installedBuildVersion)
+
+        return (runtimeInstalled != 0
+            and installedMajorVersion >= majorVersion
+            and installedMinorVersion >= minorVersion
+            and installedBuildVersion >= buildVersion)
+    except:
+        return False
+
+def installMSVCRedist():
+    url = 'http://download.microsoft.com/download/9/a/2/9a2a7e36-a8af-46c0-8a78-a5eb111eefe2/vc_redist.x86.exe'
+    downloadedFile = os.path.join(tempfile.gettempdir(), 'vc_redist.x86.exe')
+
+    print 'Downloading the MSVC 2015 redistributable...'
+    urllib.urlretrieve(url, downloadedFile)
+
+    print 'Installing the MSVC 2015 redistributable...'
+    subprocess.call([downloadedFile, '/quiet'])
+
+    os.remove(downloadedFile)
+
+def isLootApiInstalled(revision):
+    return ('loot_api' in sys.modules
+        and loot_api.WrapperVersion.revision == revision)
+
+def installLootApi(revision):
+    url = 'https://bintray.com/wrinklyninja/loot/download_file?file_path=loot_api_python-' + revision + '-win32.7z'
+    archivePath = os.path.join(tempfile.gettempdir(), 'archive.7z')
+    compiledPath = os.path.join('..', 'Mopy', 'bash', 'compiled')
+    sevenZipPath = os.path.join(compiledPath, '7z.exe')
+
+    print 'Downloading LOOT API Python wrapper from "' + url + '"...'
+    urllib.urlretrieve(url, archivePath)
+
+    print 'Extracting LOOT API Python wrapper to ' + compiledPath
+    subprocess.call([sevenZipPath, 'e', archivePath, '-y', '-o' + compiledPath, '*/loot_api.dll', '*/loot_api.pyd'])
+
+    os.remove(archivePath)
+
+if isMSVCRedistInstalled(14, 0, 24212):
+    print 'MSVC 2015 Redistributable is already installed'
+else:
+    installMSVCRedist()
+
+lootApiWrapperRevision = '4faee6b'
+if isLootApiInstalled(lootApiWrapperRevision):
+    print 'LOOT API wrapper revision %s is already installed' % lootApiWrapperRevision
+else:
+    installLootApi(lootApiWrapperRevision)
